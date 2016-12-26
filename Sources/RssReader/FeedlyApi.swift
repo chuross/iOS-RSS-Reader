7bd866bbbf6859dce6281de5debc906cd52eef13
chuross <azarasi.mo.mo@gmail.com>
2016-12-27 03:10:48 +0900

    API実装のベース部分を作った

---
+++ Sources/RssReader/FeedlyApi.swift
@@ -0,0 +1,12 @@
+//
+//  FeedlyApi.swift
+//  RssReader
+//
+//  Created by chuross on 2016/12/27.
+//  Copyright © 2016年 chuross. All rights reserved.
+//
+
+import Foundation
+
+class FeedlyApi {
+}
---
+++ Sources/RssReader/Session+Rx.swift
@@ -0,0 +1,31 @@
+//
+//  Session+Rx.swift
+//  RssReader
+//
+//  Created by chuross on 2016/12/27.
+//  Copyright © 2016年 chuross. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import RxSwift
+
+extension Session {
+
+    public static func rxResponse<T: Request>(request: T) -> Observable<T.Response> {
+        return Observable.create { observer in
+            let task = send(request) { result in
+                switch result {
+                case .success(let response):
+                    observer.onNext(response)
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create {
+                task?.cancel()
+            }
+        }
+    }
+}
